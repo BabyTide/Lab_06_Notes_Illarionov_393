5b61250c40cf4e2d7b0a1e6b93d77dd53477012a
BabyTide <vertina2234@mail.ru>
2021-12-27 19:44:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab_06_notes_illarionov_393/Note.java
@@ -0,0 +1,12 @@
+package com.example.lab_06_notes_illarionov_393;
+
+public class Note {
+
+    public String title;
+    public String content;
+
+    public String toString()
+    {
+        return title;
+    }
+}
